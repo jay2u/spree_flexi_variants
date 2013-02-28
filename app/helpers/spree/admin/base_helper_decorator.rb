d8e93b5e45cc59bb44bcaf487b56adcfd26bb5b5
Jan-Willem Korver <jwkorver@xs4all.nl>
2013-02-28 09:57:30 +0100

    fix product options error

--- app/helpers/spree/admin/base_helper_decorator.rb
+++ app/helpers/spree/admin/base_helper_decorator.rb
@@ -1,9 +1,13 @@
 module Spree
   module Admin
     BaseHelper.module_eval do
-      def link_to_remove_fields(name, f)
-        # fixes https://github.com/jsqu99/spree_flexi_variants/issues/52 by reverting back to 'old spree' version
-        f.hidden_field(:_destroy) + link_to_with_icon(:delete, name, '#', :class => 'remove_fields')
+      # renders hidden field and link to remove record using nested_attributes
+      def link_to_remove_fields(name, f, options = {})
+        name = '' if options[:no_text]
+        options[:class] = '' unless options[:class]
+        options[:class] += 'no-text with-tip' if options[:no_text]
+        url = f.object.persisted? ? [:admin, f.object] : '#'
+        link_to_with_icon('icon-trash', name, url, :class => "remove_fields #{options[:class]}", :data => {:action => 'remove'}, :title => t(:remove)) + f.hidden_field(:_destroy)
       end
     end
   end
